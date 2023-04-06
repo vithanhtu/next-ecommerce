7766d5b5097e86d731d212594e71073d29ea7ad9
vithanhtu <vithanhtu21@gmail.com>
2023-04-06 09:12:00 +0700

    finisher project

--- src/models/Products.js
+++ src/models/Products.js
@@ -1,5 +1,17 @@
 import mongoose from "mongoose";
 
+const reviewSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    name: { type: String, required: true },
+    rating: { type: Number, default: 0 },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
 const productSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
@@ -8,6 +20,7 @@ const productSchema = new mongoose.Schema(
     info: { type: String, required: true },
     img: { type: String, required: true },
     price: { type: Number, required: true },
+    reviews: [reviewSchema],
     rating: { type: Number, required: true, default: 0 },
     countInStock: { type: Number, default: 0 },
     newProduct: { type: Boolean, required: true },
